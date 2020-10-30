8dc09aca325d2804e0890d395b80abc1bd364c3f
Egor Andrianov <tuogeniy@gmail.com>
2020-10-30 23:00:39 +0400

    chore(actl-commit): fix execa import

--- packages/actl-check/src/commands/check/typecheck.ts
+++ packages/actl-check/src/commands/check/typecheck.ts
@@ -1,4 +1,4 @@
-import * as execa from 'execa'
+import execa                           from 'execa'
 import { Command }                     from '@oclif/command'
 
 import { AnnotationLevel, Conclusion } from '../../types'
