d0cd01454d6c7f03bc80c90744510fefbc8d190c
Andrew Ghostuhin <me@torinasakura.name>
2020-11-06 22:50:08 +0300

    refactor(actl-generator): remove symbol symLink
    affects: @atlantis-lab/actl-generator
    
    - add tsconfig
    - update publish flow
    - update dependencies
    
    BREAKING CHANGE:
    kdefaultHandler -> defaultHandler

--- packages/actl-generator/src/types.ts
+++ packages/actl-generator/src/types.ts
@@ -3,7 +3,7 @@ export type fn<T> = () => T | Promise<T>
 export type GeneratorResolversRouter<T> = Map<fn<T>, string[]>
 
 export interface GeneratorOptions {
-  kdefaultHandler?: fn<any> | null
+  defaultHandler?: fn<any> | null
   readonly allProperties: string[]
   readonly strategyRouter: GeneratorResolversRouter<any>
 }
