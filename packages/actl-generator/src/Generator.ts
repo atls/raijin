dccca7e75e85343ac46901067fb323492c9749de
Egor Andrianov <tuogeniy@gmail.com>
2020-11-11 16:55:01 +0400

    fix(actl-generator): properties composition & shared option links
    affects: @atlantis-lab/actl-generator

--- packages/actl-generator/src/Generator.ts
+++ packages/actl-generator/src/Generator.ts
@@ -1,7 +1,7 @@
 import { GeneratorOptions } from './types'
 
 export class Generator<T> {
-  public constructor(private readonly options: GeneratorOptions) { }
+  public constructor(private readonly options: GeneratorOptions) {}
 
   public async generate(): Promise<T> {
     const entries = []
@@ -10,18 +10,26 @@ export class Generator<T> {
       entries.push([this.options.defaultHandler, this.options.allProperties])
     }
 
-    const promises = entries.concat(Array.from(this.options.strategyRouter.entries()))
+    const promises = entries
+      .concat(Array.from(this.options.strategyRouter.entries()))
       .map(async ([handler, properties]) => {
         const rawResult = await handler()
-        return properties.reduce(
-          (result, property) => Object.assign({}, result, property ? { [property]: rawResult[property] } : null ),
-          {} as T,
-        )
+
+        return properties.reduce((result, property) => {
+          let patch = null
+
+          if (property !== undefined) {
+            patch = {
+              [property]: typeof rawResult === 'object' ? rawResult[property] : rawResult,
+            }
+          }
+
+          return { ...result, ...patch }
+        }, {} as T)
       })
 
-      const results = await Promise.all(promises)
+    const results = await Promise.all(promises)
 
-      return results.reduce((result, chunk) => ({ ...result, ...chunk }))
+    return results.reduce((result, chunk) => ({ ...result, ...chunk }), {})
   }
 }
-
