84dbffd5880a44e40e14f726c2c650b7248acd5e
Nikita Elfimov <ne@atls.team>
2021-03-26 18:52:58 +0500

    WIP: init clipanion in actl-lint
    affects: @atlantis-lab/actl-lint, @atlantis-lab/actl
    
    ISSUES CLOSED: #122

--- packages/actl-lint/src/commands/lint.ts
+++ packages/actl-lint/src/commands/lint.ts
@@ -1,32 +1,34 @@
-import { Command } from '@oclif/command'
 import execa from 'execa'
 import {
   ESLINT_CONFIG_PATH,
   ESLINT_IGNORE_PATH,
 } from '@atlantis-lab/config'
 
-export default class LintCommand extends Command {
-  static description: string = 'Run ESLint to statically analyze your code'
-  static examples: string[] = ['$ actl lint']
+import { Command, Option } from 'clipanion';
 
-  async run(): Promise<void> {
-    try {
-      await execa('eslint', [
-        '--ext',
-        'js,ts,jsx,tsx',
-        process.cwd(),
-        '--config',
-        ESLINT_CONFIG_PATH,
-        '--ignore-path',
-        ESLINT_IGNORE_PATH,
-        ...this.argv,
-      ], { stdio: 'inherit' });
-    }
-    catch (error) {
-      this.log(error.stderr);
-      if (error.exitCode !== 0) {
-        process.exit(error.exitCode === null ? 0 : error.exitCode);
-      }
-    }
+export class LintCommand extends Command {
+  async execute() {
+    this.context.stdout.write(`Hello clip`)
   }
+
+  // async run(): Promise<void> {
+  //   try {
+  //     await execa('eslint', [
+  //       '--ext',
+  //       'js,ts,jsx,tsx',
+  //       process.cwd(),
+  //       '--config',
+  //       ESLINT_CONFIG_PATH,
+  //       '--ignore-path',
+  //       ESLINT_IGNORE_PATH,
+  //       ...this.argv,
+  //     ], { stdio: 'inherit' });
+  //   }
+  //   catch (error) {
+  //     this.log(error.stderr);
+  //     if (error.exitCode !== 0) {
+  //       process.exit(error.exitCode === null ? 0 : error.exitCode);
+  //     }
+  //   }
+  // }
 }
--- packages/actl-lint/src/index.ts
+++ packages/actl-lint/src/index.ts
@@ -1,1 +1,16 @@
-export default {}
+#!/usr/bin/env node
+
+import { Cli } from 'clipanion'
+
+import { LintCommand } from './commands/lint'
+
+const [...args] = process.argv
+
+const cli = new Cli({
+  binaryLabel: `Any`,
+  binaryName: `anyco`,
+  binaryVersion: `1.0.0`,
+})
+
+cli.register(LintCommand)
+cli.runExit(args, Cli.defaultContext)
