d15b27d551a8793d5de0c4ee86751c15cc30a11f
Nikita Elfimov <ne@atls.team>
2021-03-27 17:55:40 +0500

    feat: move actl-test to clipanion
    affects: @atlantis-lab/actl-test, @atlantis-lab/actl
    
    ISSUES CLOSED: #132

--- packages/actl-test/src/commands/test.ts
+++ packages/actl-test/src/commands/test.ts
@@ -1,19 +1,23 @@
 import execa                from 'execa'
 import { JEST_CONFIG_PATH } from '@atlantis-lab/config'
-import { Command }          from '@oclif/command'
+import { Command, Option }  from 'clipanion'
 
 export default class TestCommand extends Command {
-  static description: string = 'Run tests via jest'
+  // static description: string = 'Run tests via jest'
+  //
+  // static examples: string[] = ['$ actl test']
 
-  static examples: string[] = ['$ actl test']
+  static paths = [['test']]
 
-  async run(): Promise<void> {
+  args = Option.Proxy()
+
+  async execute(): Promise<void> {
     try {
-      await execa('jest', ['--config', JEST_CONFIG_PATH, ...this.argv], {
+      await execa('jest', ['--config', JEST_CONFIG_PATH, ...this.args], {
         stdio: 'inherit',
       })
     } catch (error) {
-      this.log(error.stderr)
+      this.context.stdout.write(`${error.stderr}`)
       if (error.exitCode !== 0) {
         process.exit(error.exitCode === null ? 0 : error.exitCode)
       }
