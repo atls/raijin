130e80df0b6564d79a4152a0723cd34ec0b2274f
Nikita Elfimov <ne@atls.team>
2021-03-27 17:10:50 +0500

    feat: move actl-commit to clipanion
    affects: @atlantis-lab/actl-check, @atlantis-lab/actl-commit, @atlantis-lab/actl
    
    - Also deleted bin directories
    
    ISSUES CLOSED: #127

--- packages/actl-commit/src/index.ts
+++ packages/actl-commit/src/index.ts
@@ -1,1 +1,5 @@
-export default {}
+import CommitCommand from './commands/commit'
+
+export {
+  CommitCommand
+}
--- packages/actl/src/commands/commands.ts
+++ packages/actl/src/commands/commands.ts
@@ -1,3 +1,4 @@
 export * from '@atlantis-lab/actl-build'
 export * from '@atlantis-lab/actl-lint'
 export * from '@atlantis-lab/actl-check'
+export * from '@atlantis-lab/actl-commit'
