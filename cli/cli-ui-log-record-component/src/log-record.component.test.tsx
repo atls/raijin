33671e2a2ceab0cbb141f79d2075fde2af32fde1
Andrew Ghostuhin <me@torinasakura.name>
2021-05-20 23:45:47 +0300

    feat: remove repository field, add cli & webpack utilities

--- cli/cli-ui-log-record-component/src/log-record.component.test.tsx
+++ cli/cli-ui-log-record-component/src/log-record.component.test.tsx
@@ -8,7 +8,7 @@ const createStack = () => {
   const cwd = process.cwd()
 
   return `Error: test
-    at Object.<anonymous> (${cwd}/mctl/mctl-ui/src/log/log-record.component.test.tsx:10:12)
+    at Object.<anonymous> (${cwd}/actl/actl-ui/src/log/log-record.component.test.tsx:10:12)
     at Object.asyncJestTest (${cwd}/.yarn/cache/jest-jasmine2-npm-26.6.3-aba0c11c28-18b15901f8.zip/node_modules/jest-jasmine2/build/jasmineAsyncInstall.js:106:37)
     at ${cwd}/.yarn/cache/jest-jasmine2-npm-26.6.3-aba0c11c28-18b15901f8.zip/node_modules/jest-jasmine2/build/queueRunner.js:45:12
     at new Promise (<anonymous>)
