3114eac97c0792a96ee5bafb24138a7910f60b49
Nikita Elfimov <ne@atls.team>
2021-09-25 15:00:50 +0500

    chore(code): increase timeout for test

--- code/code-service/integration/watch.test.ts
+++ code/code-service/integration/watch.test.ts
@@ -6,6 +6,8 @@ import path      from 'path'
 
 import { watch } from '../src'
 
+jest.setTimeout(7000)
+
 const closeWatcher = (watcher): Promise<void> =>
   new Promise((resolve) =>
     watcher.close(() => {
