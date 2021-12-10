851ebe8061fffc31ad7a37f6ad7bf3e9e75ab869
Andrew Ghostuhin <me@torinasakura.name>
2021-12-10 21:26:01 +0300

    feat(plugin-test): workspace scoped tests

--- yarn/plugin-test/src/test-integration.command.ts
+++ yarn/plugin-test/src/test-integration.command.ts
@@ -1,5 +1,6 @@
-import { BaseCommand } from '@yarnpkg/cli'
-import { Option }      from 'clipanion'
+import { BaseCommand }  from '@yarnpkg/cli'
+import { PortablePath } from '@yarnpkg/fslib'
+import { Option }       from 'clipanion'
 
 class TestIntegrationCommand extends BaseCommand {
   static paths = [['test', 'integration']]
@@ -7,7 +8,20 @@ class TestIntegrationCommand extends BaseCommand {
   args: Array<string> = Option.Rest({ required: 0 })
 
   async execute() {
-    await this.cli.run(['actl', 'test:integration', ...this.args])
+    const cwd = this.cwd || process.cwd()
+    const isWorkspace = this.context.cwd !== cwd
+
+    const args = ['actl', 'test:integration']
+
+    if (isWorkspace) {
+      const scope = this.context.cwd.replace(cwd, '')
+
+      args.push(scope.startsWith('/') ? scope.substr(1) : scope)
+    }
+
+    await this.cli.run(args.concat(this.args), {
+      cwd: isWorkspace ? (cwd as PortablePath) : this.context.cwd,
+    })
   }
 }
 
--- yarn/plugin-test/src/test-unit.command.ts
+++ yarn/plugin-test/src/test-unit.command.ts
@@ -1,5 +1,6 @@
-import { BaseCommand } from '@yarnpkg/cli'
-import { Option }      from 'clipanion'
+import { BaseCommand }  from '@yarnpkg/cli'
+import { PortablePath } from '@yarnpkg/fslib'
+import { Option }       from 'clipanion'
 
 class TestUnitCommand extends BaseCommand {
   static paths = [['test', 'unit']]
@@ -7,7 +8,20 @@ class TestUnitCommand extends BaseCommand {
   args: Array<string> = Option.Rest({ required: 0 })
 
   async execute() {
-    await this.cli.run(['actl', 'test:unit', ...this.args])
+    const cwd = this.cwd || process.cwd()
+    const isWorkspace = this.context.cwd !== cwd
+
+    const args = ['actl', 'test:unit']
+
+    if (isWorkspace) {
+      const scope = this.context.cwd.replace(cwd, '')
+
+      args.push(scope.startsWith('/') ? scope.substr(1) : scope)
+    }
+
+    await this.cli.run(args.concat(this.args), {
+      cwd: isWorkspace ? (cwd as PortablePath) : this.context.cwd,
+    })
   }
 }
 
