f9be387766b21b1c515dc556c564d32ca033ad5b
Andrew Ghostuhin <me@torinasakura.name>
2021-12-31 18:02:37 +0300

    feat(yarn): remove app

--- yarn/plugin-app/src/index.ts
+++
@@ -1,9 +0,0 @@
-export { plugin as default } from './app.plugin'
-
-export * from './app-library-bulid.command'
-export * from './app-renderer-build.command'
-export * from './app-renderer-dev.command'
-export * from './app-service-build.command'
-export * from './app-service-dev.command'
-export * from './app-pack-source.command'
-export * from './app-pack.command'
