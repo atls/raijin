0dc1c98c1e55834b20c4ded742b1d7bd906df4e6
Andrew Ghostuhin <me@torinasakura.name>
2022-02-17 15:11:06 +0300

    debug: checking schematics

---
+++ schematics/schematics/src/project/migrations/update-1643063153411/library-workflows-remove-run.migration.ts
@@ -0,0 +1,11 @@
+import { chain }            from '@angular-devkit/schematics'
+
+import { updateFileInTree } from '@monstrs/schematics-utils'
+
+const removeRunFromVersionWorkflow = updateFileInTree('.github/workflows/version.yaml', (file) =>
+  file.replace('run version patch', 'version patch'))
+
+const removeRunFromPublishWorkflow = updateFileInTree('.github/workflows/publish.yaml', (file) =>
+  file.replace('run npm publish', 'npm publish'))
+
+export default () => chain([removeRunFromVersionWorkflow, removeRunFromPublishWorkflow])
