be88449a002288aa71179fbb6ed7ee71ad930673
Nikita Elfimov <me@tfk.name>
2022-10-31 00:57:26 +0500

    feat(badges): rework dependencies traversing

--- yarn/plugin-badges/src/badges.command.ts
+++ yarn/plugin-badges/src/badges.command.ts
@@ -1,8 +1,14 @@
 import { BaseCommand }     from '@yarnpkg/cli'
+import { WorkspaceRequiredError }     from '@yarnpkg/cli'
 import { StreamReport }    from '@yarnpkg/core'
 import { Configuration }   from '@yarnpkg/core'
+import { Project }   from '@yarnpkg/core'
+import { structUtils }   from '@yarnpkg/core'
+import { miscUtils }   from '@yarnpkg/core'
+import { LocatorHash }   from '@yarnpkg/core'
+import { Package }   from '@yarnpkg/core'
+import { Workspace }   from '@yarnpkg/core'
 
-import { exec }            from 'child-process-promise'
 import { readFileAsync }   from 'fs-extra-promise'
 import { writeFileAsync }  from 'fs-extra-promise'
 import { join }            from 'path'
@@ -23,6 +29,12 @@ class BadgesCommand extends BaseCommand {
 
   async execute() {
     const configuration = await Configuration.find(this.context.cwd, this.context.plugins)
+    const { project, workspace } = await Project.find(configuration, this.context.cwd)
+
+    if (!workspace)
+      throw new WorkspaceRequiredError(project.cwd, this.context.cwd);
+
+    await project.restoreInstallState();
 
     const commandReport = await StreamReport.start(
       {
@@ -35,13 +47,72 @@ class BadgesCommand extends BaseCommand {
 
           progress.start()
 
-          const getVersion = async (pkg) => {
-            try {
-              const raw = await exec(`yarn info ${pkg} --json`)
-              return JSON.parse(raw.stdout).children.Version
-            } catch (e) {
-              return ''
+          const traverseWorkspace = (workspace: Workspace) => {
+            const initialHash = workspace.anchoredLocator.locatorHash;
+
+            const seen = new Map<LocatorHash, Package>();
+            const pass = [initialHash];
+
+            while (pass.length > 0) {
+              const hash = pass.shift()!;
+              if (seen.has(hash))
+                continue;
+
+              const pkg = project.storedPackages.get(hash);
+              if (typeof pkg === `undefined`)
+                throw new Error(`Assertion failed: Expected the package to be registered`);
+
+              seen.set(hash, pkg);
+
+              if (structUtils.isVirtualLocator(pkg))
+                pass.push(structUtils.devirtualizeLocator(pkg).locatorHash);
+
+              if (hash !== initialHash)
+                continue;
+
+              for (const dependency of pkg.dependencies.values()) {
+                const resolution = project.storedResolutions.get(dependency.descriptorHash);
+                if (typeof resolution === `undefined`)
+                  throw new Error(`Assertion failed: Expected the resolution to be registered`);
+
+                pass.push(resolution);
+              }
             }
+
+            return seen.values();
+          };
+
+          const traverseAllWorkspaces = () => {
+            const aggregate = new Map<LocatorHash, Package>();
+
+            for (const workspace of project.workspaces)
+              for (const pkg of traverseWorkspace(workspace))
+                aggregate.set(pkg.locatorHash, pkg);
+
+            return aggregate.values();
+          };
+
+          const lookupSet = traverseAllWorkspaces()
+          const sortedLookup = miscUtils.sortMap([...lookupSet], pkg => {
+            return structUtils.stringifyLocator(pkg)
+          })
+
+          const getVersion = async (name) => {
+            const expectedDescriptor = structUtils.parseDescriptor(name)
+
+            const selection = sortedLookup.filter(pkg => {
+              if (pkg.scope === expectedDescriptor.scope && pkg.name === expectedDescriptor.name) {
+                return true
+              }
+
+              return false
+            })
+
+            if (selection.length > 0) {
+              return selection.shift()!.version
+            }
+
+            return ''
           }
 
           const readmePath = join(process.cwd(), 'README.md')
