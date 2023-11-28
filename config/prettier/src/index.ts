adf8129535023a802e952ca4d98fd6214cf6b2ca
Nikita Elfimov <nelfimov@mail.ru>
2023-11-28 18:32:41 +0300

    feat: prettier imports sort plugin, generating json file as well

--- config/prettier/src/index.ts
+++ config/prettier/src/index.ts
@@ -5,4 +5,9 @@ export default {
   jsxSingleQuote: true,
   trailingComma: 'es5',
   printWidth: 100,
+  importOrder: ['^react(.*)$', '^@pages/(.*)', '^@fragments/(.*)$', '^@ui/(.*)$', '^@stores/(.*)$', '^@shared/(.*)$', '^@globals/(.*)$', '^@utils/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderCaseInsensitive: true,
+  importOrderGroupNamespaceSpecifiers: true,
+  plugins: [require.resolve('@trivago/prettier-plugin-sort-imports')]
 }
