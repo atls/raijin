71aad7fd89c6f892c660d44874b78970130ca4c5
Nikita Elfimov <nelfimov@mail.ru>
2023-12-15 15:35:07 +0300

    feat: remove trivago plugin

--- config/prettier/src/index.ts
+++ config/prettier/src/index.ts
@@ -5,9 +5,4 @@ export default {
   jsxSingleQuote: true,
   trailingComma: 'es5',
   printWidth: 100,
-  importOrder: ['^react(.*)$', '^@pages/(.*)', '^@fragments/(.*)$', '^@ui/(.*)$', '^@stores/(.*)$', '^@shared/(.*)$', '^@globals/(.*)$', '^@utils/(.*)$', '^[./]'],
-  importOrderSeparation: true,
-  importOrderCaseInsensitive: true,
-  importOrderGroupNamespaceSpecifiers: true,
-  plugins: [require.resolve('@trivago/prettier-plugin-sort-imports')]
 }
