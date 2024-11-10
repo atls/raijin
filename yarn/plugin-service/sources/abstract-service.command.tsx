7c0819368925dc6bf802ee40d74934fc85a30eb3
Nikita Elfimov <nelfimov@mail.ru>
2024-11-10 11:31:13 +0300

    feat(yarn): service

--- yarn/plugin-service/sources/abstract-service.command.tsx
+++ yarn/plugin-service/sources/abstract-service.command.tsx
@@ -1,54 +1,55 @@
 import type { ServiceLogRecord } from '@atls/code-service'
-import type { StreamReport }     from '@yarnpkg/core'
 
 import { SeverityNumber }        from '@monstrs/logger'
 import { BaseCommand }           from '@yarnpkg/cli'
-import { MessageName }           from '@yarnpkg/core'
 import { Option }                from 'clipanion'
 import React                     from 'react'
 
 import { ErrorInfo }             from '@atls/cli-ui-error-info-component'
 import { LogRecord }             from '@atls/cli-ui-log-record-component'
-import { renderStatic }          from '@atls/cli-ui-renderer'
+import { renderStatic }          from '@atls/cli-ui-renderer-static-component'
 
 export abstract class AbstractServiceCommand extends BaseCommand {
   showWarnings = Option.Boolean('-w,--show-warnings', false)
 
-  renderLogRecord(logRecord: ServiceLogRecord, report: StreamReport): void {
+  renderLogRecord(logRecord: ServiceLogRecord): void {
     if (logRecord instanceof Error) {
-      renderStatic(<ErrorInfo error={logRecord} />, process.stdout.columns - 12)
+      renderStatic(<ErrorInfo error={logRecord} />)
         .split('\n')
         .forEach((line) => {
-          report.reportError(MessageName.UNNAMED, line)
+          console.log(line) // eslint-disable-line no-console
         })
     } else if ('severityNumber' in logRecord && 'record' in logRecord) {
-      renderStatic(<ErrorInfo error={logRecord.record as Error} />, process.stdout.columns - 12)
+      renderStatic(<ErrorInfo error={logRecord.record as Error} />)
         .split('\n')
         .forEach((line) => {
           if (logRecord.severityNumber === SeverityNumber.WARN) {
             if (this.showWarnings) {
-              report.reportWarning(MessageName.UNNAMED, line)
+              console.log(line) // eslint-disable-line no-console
             }
           } else {
-            report.reportError(MessageName.UNNAMED, line)
+            console.log(line) // eslint-disable-line no-console
           }
         })
     } else if ('severityNumber' in logRecord) {
-      renderStatic(<LogRecord {...logRecord} />, process.stdout.columns - 12)
+      renderStatic(<LogRecord {...logRecord} />)
         .split('\n')
         .forEach((line) => {
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
           if (logRecord.severityNumber! <= SeverityNumber.INFO) {
-            report.reportInfo(MessageName.UNNAMED, line)
+            console.log(line) // eslint-disable-line no-console
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
           } else if (logRecord.severityNumber! <= SeverityNumber.WARN) {
             if (this.showWarnings) {
-              report.reportWarning(MessageName.UNNAMED, line)
+              console.log(line) // eslint-disable-line no-console
             }
           } else {
-            report.reportError(MessageName.UNNAMED, line)
+            console.log(line) // eslint-disable-line no-console
           }
         })
     } else {
-      report.reportWarning(MessageName.UNNAMED, `Unknown record type: ${JSON.stringify(logRecord)}`)
+      // eslint-disable-next-line no-console
+      console.log(`Unknown record type: ${JSON.stringify(logRecord)}`)
     }
   }
 }
--- yarn/plugin-service/sources/service-build.command.tsx
+++ yarn/plugin-service/sources/service-build.command.tsx
@@ -1,53 +1,84 @@
 import { Configuration }          from '@yarnpkg/core'
-import { StreamReport }           from '@yarnpkg/core'
 import { Project }                from '@yarnpkg/core'
-import { Option }                 from 'clipanion'
+import { Filename }               from '@yarnpkg/fslib'
+import { scriptUtils }            from '@yarnpkg/core'
+import { execUtils }              from '@yarnpkg/core'
+import { xfs }                    from '@yarnpkg/fslib'
+import { render }                 from 'ink'
 import React                      from 'react'
 
-import { SpinnerProgress }        from '@atls/yarn-run-utils'
+import { ErrorInfo }              from '@atls/cli-ui-error-info-component'
+import { ServiceProgress }        from '@atls/cli-ui-service-progress-component'
+import { Service }                from '@atls/code-service'
+import { renderStatic }           from '@atls/cli-ui-renderer-static-component'
 
 import { AbstractServiceCommand } from './abstract-service.command.jsx'
 
-class ServiceBuildCommand extends AbstractServiceCommand {
+export class ServiceBuildCommand extends AbstractServiceCommand {
   static paths = [['service', 'build']]
 
   showWarnings = Option.Boolean('-w,--show-warnings', false)
 
-  async execute(): Promise<number> {
+  override async execute(): Promise<number> {
+    const nodeOptions = process.env.NODE_OPTIONS ?? ''
+
+    if (nodeOptions.includes(Filename.pnpCjs) && nodeOptions.includes(Filename.pnpEsmLoader)) {
+      return this.executeRegular()
+    }
+
+    return this.executeProxy()
+  }
+
+  async executeProxy(): Promise<number> {
     const configuration = await Configuration.find(this.context.cwd, this.context.plugins)
     const { project } = await Project.find(configuration, this.context.cwd)
 
-    const commandReport = await StreamReport.start(
-      {
-        stdout: this.context.stdout,
-        configuration,
-      },
-      async (report) => {
-        await report.startTimerPromise('Service build', async () => {
-          const progress = new SpinnerProgress(this.context.stdout, configuration)
+    const args: Array<string> = []
 
-          try {
-            progress.start()
+    if (this.showWarnings) {
+      args.push('-s')
+    }
 
-            // @ts-expect-error any
-            const result = await new ServiceWorker(project.cwd).run(this.context.cwd)
+    const binFolder = await xfs.mktempPromise()
 
-            progress.end()
+    const { code } = await execUtils.pipevp('yarn', ['service', 'build', ...args], {
+      cwd: this.context.cwd,
+      stdin: this.context.stdin,
+      stdout: this.context.stdout,
+      stderr: this.context.stderr,
+      env: await scriptUtils.makeScriptEnv({ binFolder, project }),
+    })
 
-            result.forEach((log) => {
-              this.renderLogRecord(log, report)
-            })
-          } catch (error) {
-            progress.end()
+    return code
+  }
 
-            this.renderLogRecord(error as Error, report)
-          }
-        })
+  async executeRegular(): Promise<number> {
+    const service = await Service.initialize(this.context.cwd)
+
+    const { clear } = render(<ServiceProgress service={service} />)
+
+    try {
+      const logRecords = await service.build()
+
+      logRecords.forEach((logRecord) => {
+        this.renderLogRecord(logRecord)
+      })
+
+      return 0
+    } catch (error) {
+      if (error instanceof Error) {
+        renderStatic(<ErrorInfo error={error} />)
+          .split('\n')
+          .forEach((line) => {
+            console.error(line) // eslint-disable-line no-console
+          })
+      } else {
+        console.error(error) // eslint-disable-line no-console
       }
-    )
 
-    return commandReport.exitCode()
+      return 1
+    } finally {
+      clear()
+    }
   }
 }
-
-export { ServiceBuildCommand }
