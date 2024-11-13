1de5983d7557229b015bf7c0d84fd809c177c36f
Nikita Elfimov <nelfimov@mail.ru>
2024-11-13 18:55:37 +0300

    fix(yarn): after install hook to check for directories

--- yarn/plugin-tools/sources/hooks/after-all-installed.hook.ts
+++ yarn/plugin-tools/sources/hooks/after-all-installed.hook.ts
@@ -2,7 +2,6 @@ import type { Project }          from '@yarnpkg/core'
 import type { SpawnSyncReturns } from 'node:child_process'
 
 import { spawnSync }             from 'node:child_process'
-import { platform }              from 'node:os'
 
 import { ppath }                 from '@yarnpkg/fslib'
 import { xfs }                   from '@yarnpkg/fslib'
@@ -23,10 +22,24 @@ const hooksExists = (): boolean => {
 }
 
 export const afterAllInstalled = async (project: Project): Promise<void> => {
+  if (process.env.GITHUB_ACTIONS) {
+    console.log('AFTER INSTALL HOOK: Execution in GitHub Action')
+    return
+  }
+
   if (process.env.IMAGE_PACK) return
 
   try {
     const target = ppath.join(project.cwd, '.config/husky')
+    const legacyTarget = ppath.join(target, '_')
+
+    if (await xfs.existsPromise(legacyTarget)) {
+      await xfs.removePromise(target)
+    }
+
+    if (!(await xfs.existsPromise(target))) {
+      await xfs.mkdirPromise(target, { recursive: true })
+    }
 
     await xfs.writeFilePromise(ppath.join(target, 'commit-msg'), hook('yarn commit message lint'), {
       mode: 0o755,
