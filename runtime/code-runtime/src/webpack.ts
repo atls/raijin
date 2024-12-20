da795bf5d4d9fe77334c6b6466ee7228ef25656a
Nikita Elfimov <nelfimov@mail.ru>
2024-12-20 14:37:32 +0300

    feat(webpack): use custom proto loader

--- runtime/code-runtime/src/webpack.ts
+++ runtime/code-runtime/src/webpack.ts
@@ -7,6 +7,6 @@ const require = createRequire(import.meta.url)
 const tsLoaderPath = require.resolve('ts-loader')
 const nodeLoaderPath = require.resolve('node-loader')
 const nullLoaderPath = require.resolve('null-loader')
-const protoLoaderPath = require.resolve('protobufjs-loader')
+const protoLoaderPath = require.resolve('@atls/webpack-proto-imports-loader')
 
 export { webpack, tsLoaderPath, nodeLoaderPath, nullLoaderPath, protoLoaderPath }
