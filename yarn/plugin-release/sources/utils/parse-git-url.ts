cfa87e42feddc066f1a81f64d268c2af16b73f12
Nikita Elfimov <nelfimov@mail.ru>
2025-01-01 16:53:48 +0300

    fix(yarn): release

--- yarn/plugin-release/sources/utils/parse-git-url.ts
+++ yarn/plugin-release/sources/utils/parse-git-url.ts
@@ -3,6 +3,7 @@ import assert from 'node:assert/strict'
 type ParseGitHubUrl = (repositoryUrl: string) => { organization: string; repository: string }
 
 export const parseGitHubUrl: ParseGitHubUrl = (repositoryUrl) => {
+  console.debug('Repository URL:', repositoryUrl)
   const match = repositoryUrl.match(/github\.com[/:](.+?)\/(.+?)(?:\.git|$)/)
 
   assert.ok(match, 'URL does not match pattern')
