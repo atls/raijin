b55d92d428f6852f4fe5136f202f461c44bee4c1
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-02-05 18:28:38 +0300

    refactor(schematics): rm unused

--- schematics/schematics/src.bak/utils/tsconfig.utils.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-
-import { updateJsonInTree } from './json.utils.js'
-
-export const updateTsConfigInTree = (compilerOptions: object) =>
-  updateJsonInTree('tsconfig.json', (tsconfig) => ({
-    ...tsconfig,
-    compilerOptions,
-  }))
