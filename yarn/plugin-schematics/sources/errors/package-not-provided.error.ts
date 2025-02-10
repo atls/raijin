f54d7503b97499d2cb24bb71d4654cad62bed3d7
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-02-10 15:35:26 +0300

    feat(yarn): import code function from schematics

--- yarn/plugin-schematics/sources/errors/package-not-provided.error.ts
+++
@@ -1,7 +0,0 @@
-const SCHEMATICS_PACKAGE_NAME = '@atls/schematics'
-
-export class PackageNotProvidedError extends Error {
-  constructor() {
-    super(`Package ${SCHEMATICS_PACKAGE_NAME} not provided`)
-  }
-}
--- yarn/plugin-schematics/sources/getters/stream-report-callback.getter.ts
+++ yarn/plugin-schematics/sources/getters/stream-report-callback.getter.ts
@@ -1,31 +1,21 @@
 /* eslint-disable no-console */
 
-import '@atls/schematics'
+import type { StreamReport } from "@yarnpkg/core";
 
-import type { StreamReport }       from '@yarnpkg/core'
+import { runSchematicHelper } from "@atls/schematics";
 
-import { PackageNotProvidedError } from '../errors/index.js'
-
-type StreamReportCallbackType = Parameters<typeof StreamReport.start>[1]
+type StreamReportCallbackType = Parameters<typeof StreamReport.start>[1];
 
 export const getStreamReportCallback = async (
   options: Record<string, string>
 ): Promise<StreamReportCallbackType> => {
-  const atlsSchematics = await import('@atls/schematics')
-
-  if (!atlsSchematics) {
-    throw new PackageNotProvidedError()
-  }
-
-  const { runSchematicHelper } = atlsSchematics
-
   const streamReportCallback = async (report: StreamReport): Promise<void> => {
     try {
-      await runSchematicHelper('project', options)
+      await runSchematicHelper("project", options);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
-  return streamReportCallback
-}
+  return streamReportCallback;
+};
