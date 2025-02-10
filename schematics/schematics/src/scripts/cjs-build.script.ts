3bbc6e10ead44e90e204fdd3e0de31ae2b305d2f
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-02-10 13:42:25 +0300

    refactor(schematics): structure

---
+++ schematics/schematics/src/scripts/cjs-build.script.ts
@@ -0,0 +1,24 @@
+/* eslint-disable no-console */
+
+import { join } from "path";
+import { fileURLToPath } from "url";
+
+import { esbuildBuildStep } from "./build-steps/index.js";
+import { getAllFiles } from "./getters/index.js";
+
+const dir = fileURLToPath(new URL(".", import.meta.url));
+const srcDir = join(dir, "../schematic/");
+const outDir = join(dir, "../../dist/");
+
+const allFiles = getAllFiles(srcDir);
+
+try {
+  await esbuildBuildStep({ allFiles, outDir });
+} catch (e: unknown) {
+  const error = e as Error;
+
+  console.error("Schematic build error!");
+  console.error(error.message);
+}
+
+console.info("Schematic build successed");
