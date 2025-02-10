31dab2a0cabe131c440969867728964a77c13ede
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-02-10 16:04:16 +0300

    feat(runtime): reexport tsconfig:

--- runtime/code-runtime/src/index.ts
+++ runtime/code-runtime/src/index.ts
@@ -4,3 +4,5 @@ export * as svgr from './svgr.js'
 export * as eslint from './eslint.js'
 export * as webpack from './webpack.js'
 export * as typescript from './typescript.js'
+
+export { default as tsConfig } from '@atls/config-typescript'
