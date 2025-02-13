30041f6f88f6ddaa8090d964d55a86068dec5d8c
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-02-13 20:20:28 +0300

    refactor(yarn): schematics plugin WIP

--- yarn/plugin-schematics/sources/commands/generate-project.command.tsx
+++ yarn/plugin-schematics/sources/commands/generate-project.command.tsx
@@ -1,42 +1,52 @@
 /* eslint-disable */
 
-import { BaseCommand }             from '@yarnpkg/cli'
-import { Configuration }           from '@yarnpkg/core'
-import { Project }                 from '@yarnpkg/core'
-import { StreamReport }            from '@yarnpkg/core'
-import { Option }                  from 'clipanion'
+import { BaseCommand } from "@yarnpkg/cli";
+import { Configuration } from "@yarnpkg/core";
+import { Project } from "@yarnpkg/core";
+import { StreamReport } from "@yarnpkg/core";
+import { Option } from "clipanion";
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
 
-import { getStreamReportCallback } from '../getters/index.js'
-import { getStreamReportOptions }  from '../getters/index.js'
-import { getCollectionPath }       from '../getters/index.js'
+import { exec } from "node:child_process";
+
+import { getStreamReportCallback } from "../getters/index.js";
+import { getStreamReportOptions } from "../getters/index.js";
+import { getCollectionPath } from "../getters/index.js";
 
 export class GenerateProjectCommand extends BaseCommand {
-  static override paths = [['generate', 'project']]
+  static override paths = [["generate", "project"]];
 
-  type = Option.String('-t,--type', 'project')
+  type = Option.String("-t,--type", "project");
 
   async execute() {
-    const configuration = await Configuration.find(this.context.cwd, this.context.plugins)
+		const testContentPath = join(import.meta.dirname, 'test-content.txt')
+		console.log(testContentPath)
+		const content = await readFile(testContentPath, 'utf8')
+		console.log(content)
+		console.log(testFile)
 
-    const collectionPath = await getCollectionPath()
-
-    const allowedTypes = ['libraries', 'project']
-
-    if (!allowedTypes.includes(this.type)) {
-      throw new Error(`Allowed only ${allowedTypes.join(', ')} types`)
-    }
-
-    const options = {
-      type: this.type,
-      cwd: process.cwd(),
-      collectionPath,
-    }
-
-    const streamReportOptions = getStreamReportOptions(this, configuration)
-    const streamReportCallback = await getStreamReportCallback(options)
-
-    const commandReport = await StreamReport.start(streamReportOptions, streamReportCallback)
-
-    return commandReport.exitCode()
+    // const configuration = await Configuration.find(this.context.cwd, this.context.plugins)
+    //
+    // const collectionPath = await getCollectionPath()
+    //
+    // const allowedTypes = ['libraries', 'project']
+    //
+    // if (!allowedTypes.includes(this.type)) {
+    //   throw new Error(`Allowed only ${allowedTypes.join(', ')} types`)
+    // }
+    //
+    // const options = {
+    //   type: this.type,
+    //   cwd: process.cwd(),
+    //   collectionPath,
+    // }
+    //
+    // const streamReportOptions = getStreamReportOptions(this, configuration)
+    // const streamReportCallback = await getStreamReportCallback(options)
+    //
+    // const commandReport = await StreamReport.start(streamReportOptions, streamReportCallback)
+    //
+    // return commandReport.exitCode()
   }
 }
--- yarn/plugin-schematics/sources/plugins/schematics.plugin.ts
+++ yarn/plugin-schematics/sources/plugins/schematics.plugin.ts
@@ -2,6 +2,38 @@ import type { Plugin }            from '@yarnpkg/core'
 
 import { GenerateProjectCommand } from '../commands/index.js'
 
+import { readdirSync, readFileSync, statSync } from "fs";
+import { join, relative } from "path";
+
+// Путь к директории с файлами (из другого пакета)
+const SCHEMATICS_PATH = join(import.meta.dirname, "../schematics");
+
+// Функция рекурсивного поиска файлов без расширений
+function getAllFiles(dirPath: string): Record<string, string> {
+  let files: Record<string, string> = {};
+
+  for (const file of readdirSync(dirPath)) {
+    const fullPath = join(dirPath, file);
+    if (statSync(fullPath).isDirectory()) {
+      Object.assign(files, getAllFiles(fullPath));
+    } else {
+      const relativePath = relative(SCHEMATICS_PATH, fullPath);
+      files[relativePath] = readFileSync(fullPath, "utf-8");
+    }
+  }
+
+  return files;
+}
+
+// Встроенные файлы в бандл
+const SCHEMATICS_FILES = getAllFiles(SCHEMATICS_PATH);
+
 export const schematicsPlugin: Plugin = {
+	hooks: {
+    async afterAllInstalled() {
+   console.log("Running schematics build after bundle...");
+      console.log("Available schematics files:", Object.keys(SCHEMATICS_FILES));
+		}
+	},
   commands: [GenerateProjectCommand],
 }
