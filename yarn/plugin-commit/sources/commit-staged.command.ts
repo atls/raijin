31f95d1a36a787ec6f45c61a0004402219f3312b
Nikita Elfimov <nelfimov@mail.ru>
2025-04-09 09:07:28 +0300

    fix(code-commit): max arg length (#502)

--- yarn/plugin-commit/sources/commit-staged.command.ts
+++ yarn/plugin-commit/sources/commit-staged.command.ts
@@ -1,25 +1,35 @@
+import { execSync }    from 'node:child_process'
+
 import { BaseCommand } from '@yarnpkg/cli'
 import { Option }      from 'clipanion'
 import lintStaged      from 'lint-staged'
 
-const config = {
+const config: lintStaged.Config = {
   '*.{yml,yaml,json,graphql,md}': 'yarn format',
   '*.{js,mjs,cjs,jsx,ts,tsx}': ['yarn format', 'yarn lint'],
-  '*.{ts,tsx}': ['yarn typecheck'],
-  '*.{tsx,ts}': ['yarn test unit'],
+  '*.{ts,tsx}': 'yarn typecheck',
+  '*.{test,spec}.{ts,tsx}': 'yarn test unit',
 }
 
 export class CommitStagedCommand extends BaseCommand {
-  static paths = [['commit', 'staged']]
+  static override paths = [['commit', 'staged']]
 
   args: Array<string> = Option.Rest({ required: 0 })
 
   async execute(): Promise<number> {
     try {
+      let safeMaxArgLength = 8190
+
+      try {
+        const ARG_MAX = parseInt(execSync('getconf ARG_MAX', { encoding: 'utf-8' }), 10)
+
+        safeMaxArgLength = Math.floor(ARG_MAX * 0.5)
+      } catch {}
+
       // @ts-expect-error: Fix import
       const passed = await lintStaged({
         config,
-        debug: false,
+        maxArgLength: safeMaxArgLength,
       })
 
       return passed ? 0 : 1
