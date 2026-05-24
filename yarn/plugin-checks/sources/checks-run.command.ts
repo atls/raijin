0342c3638cd401c65f6bac651eb5ecc4829e1f8d
Andrew Ghostuhin <me@torinasakura.name>
2026-05-25 01:06:29 +0300

    fix(common): avoid linux pnp typecheck hang

--- yarn/plugin-checks/sources/checks-run.command.ts
+++ yarn/plugin-checks/sources/checks-run.command.ts
@@ -25,13 +25,22 @@ class ChecksRunCommand extends BaseCommand {
         configuration,
       },
       async (report) => {
-        await this.runCheck(project, project.cwd, ['typecheck'], report)
-        await this.runCheck(project, project.cwd, ['lint'], report)
+        if ((await this.runCheck(project, project.cwd, ['typecheck'], report)) !== 0) {
+          return
+        }
 
-        await Promise.allSettled([
+        if ((await this.runCheck(project, project.cwd, ['lint'], report)) !== 0) {
+          return
+        }
+
+        const testResults = await Promise.all([
           this.runCheck(project, project.cwd, ['test', 'unit'], report),
           this.runCheck(project, project.cwd, ['test', 'integration'], report),
         ])
+
+        if (testResults.some((code) => code !== 0)) {
+          return
+        }
 
         await this.runCheck(project, project.cwd, ['release'], report)
       }
@@ -45,7 +54,7 @@ class ChecksRunCommand extends BaseCommand {
     cwd: PortablePath,
     args: Array<string>,
     report: StreamReport
-  ): Promise<void> {
+  ): Promise<number> {
     try {
       const shouldAppendChanged =
         this.changed &&
@@ -66,6 +75,8 @@ class ChecksRunCommand extends BaseCommand {
       if (code !== 0) {
         report.reportError(MessageName.UNNAMED, `Run check ${args.join(' ')} failed: ${code}`)
       }
+
+      return code
     } catch (error) {
       report.reportError(
         MessageName.UNNAMED,
@@ -73,6 +84,8 @@ class ChecksRunCommand extends BaseCommand {
           error instanceof Error ? error.message : (error as string)
         }`
       )
+
+      return 1
     }
   }
 }
