d17336c3b54feb90e0c7f695e6e4b4364f6c4909
Andrew Ghostuhin <me@torinasakura.name>
2026-07-02 20:09:32 +0300

    fix(raijin): restore public initializer upgrade path

--- yarn/raijin/src/initializer/interface.ts
+++ yarn/raijin/src/initializer/interface.ts
@@ -1,11 +1,13 @@
 import type { FetchLike }                  from '../runtime/download.js'
 import type { YarnCommandRunner }          from '../yarn/runner.js'
 import type { RaijinScaffoldTypeSelector } from './scaffold.js'
+import type { RaijinSchematicInstaller }   from './schematic.js'
 
 export interface RunRaijinInitializerOptions {
   argv?: Array<string>
   cwd?: string
   fetchImpl?: FetchLike
+  installSchematicArtifact?: RaijinSchematicInstaller
   runYarnCommand?: YarnCommandRunner
   selectScaffoldType?: RaijinScaffoldTypeSelector
 }
