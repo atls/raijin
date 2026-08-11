1a91449b412d0b90eb951067850aeec088b8565c
Andrew Ghostuhin <me@torinasakura.name>
2026-08-12 00:52:56 +0300

    refactor(raijin): place icon adapters by provider

--- yarn/plugin-generate/sources/commands/icons.ts
+++ yarn/plugin-generate/sources/commands/icons.ts
@@ -1,29 +1,18 @@
 import type { WorkspaceCommandContext } from '@atls/raijin/commands'
-import type { PortablePath }            from '@yarnpkg/fslib'
 
 import { BaseCommand }                  from '@yarnpkg/cli'
 import { Option }                       from 'clipanion'
 
 import { generate }                     from '@atls/raijin/application/icons/generation'
-import { createCommandInput }           from '@atls/raijin/commands'
-import { toCommandArguments }           from '@atls/raijin/commands'
 import { toNativeCwd }                  from '@atls/raijin/commands'
-import { createOutputReplacer }         from '@atls/raijin/infrastructure/providers/icons/node'
-import { createSourceReader }           from '@atls/raijin/infrastructure/providers/icons/node'
-import { createTransformer }            from '@atls/raijin/infrastructure/providers/icons/svgr'
+import { createOutputReplacer }         from '@atls/raijin/infrastructure/providers/node/icons'
+import { createSourceReader }           from '@atls/raijin/infrastructure/providers/node/icons'
+import { createTransformer }            from '@atls/raijin/infrastructure/providers/svgr/icons'
+import { createFormatter }              from '@atls/raijin/infrastructure/providers/yarn/icons'
+import { createLinter }                 from '@atls/raijin/infrastructure/providers/yarn/icons'
 
 import { presentIconGeneration }        from '../presenters/icons.js'
 import { presentIconGenerationError }   from '../presenters/icons.js'
-
-const createGeneratedArguments = (cwd: PortablePath, files: ReadonlyArray<string>): Array<string> =>
-  toCommandArguments(
-    createCommandInput({
-      cwd,
-      source: 'generated',
-      targets: Array.from(files),
-    }),
-    cwd
-  )
 
 export class GenerateIconsCommand extends BaseCommand {
   static override paths = [['ui', 'icons', 'generate']]
@@ -39,19 +28,13 @@ export class GenerateIconsCommand extends BaseCommand {
   override async execute(): Promise<number> {
     const { executionCwd, yarn } = this.context.invocation
     const cwd = toNativeCwd(executionCwd)
-    const createArguments = (files: ReadonlyArray<string>) =>
-      createGeneratedArguments(executionCwd, files)
 
     try {
       const result = await generate(
         { cwd, native: this.native },
         {
-          formatter: {
-            format: async (files) => yarn.execute(['format', ...createArguments(files)]),
-          },
-          linter: {
-            lint: async (files) => yarn.execute(['lint', '--fix', ...createArguments(files)]),
-          },
+          formatter: createFormatter(executionCwd, yarn.execute),
+          linter: createLinter(executionCwd, yarn.execute),
           output: createOutputReplacer(),
           sources: createSourceReader(),
           transformer: createTransformer(cwd),
---
+++ yarn/raijin/src/infrastructure/providers/yarn/icons/index.ts
@@ -0,0 +1,2 @@
+export { create as createFormatter } from './format.js'
+export { create as createLinter }    from './lint.js'
