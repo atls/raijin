d817a0f94990ec2367111267f60d71ccce73e459
Andrew Ghostuhin <me@torinasakura.name>
2026-09-20 02:36:25 +0300

    test(raijin): respect native Angular template line endings

--- packages/raijin/src/infrastructure/generation/project/angular/tests/scaffold.test.ts
+++ packages/raijin/src/infrastructure/generation/project/angular/tests/scaffold.test.ts
@@ -6,6 +6,7 @@ import { readFile }                   from 'node:fs/promises'
 import { readdir }                    from 'node:fs/promises'
 import { rm }                         from 'node:fs/promises'
 import { writeFile }                  from 'node:fs/promises'
+import { EOL }                        from 'node:os'
 import { dirname }                    from 'node:path'
 import { join }                       from 'node:path'
 import { relative }                   from 'node:path'
@@ -65,7 +66,7 @@ const assertBaseline = async (target: string): Promise<Array<string>> => {
     if (path === 'tsconfig.json') {
       assert.deepEqual(JSON.parse(actual[path]), JSON.parse(content))
     } else {
-      assert.equal(actual[path], content, path)
+      assert.equal(actual[path], content.replace(/\r?\n/gu, EOL), path)
     }
   }
 
